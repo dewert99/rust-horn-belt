b12aa703a7004d6ed21d8c7eace01feb6cb0b4b4
松下祐介 <y.skm24t@gmail.com>
2021-03-17 00:53:39 +0900

    more in util/update.v

--- theories/util/update.v
+++ theories/util/update.v
@@ -1,12 +1,24 @@
+From iris.bi Require Import updates.
 From iris.proofmode Require Import tactics.
-From iris.base_logic Require Import invariants.
 From lrust.util Require Import basic.
 
 (** * Utility for Multi-Step-Taking Updates *)
 
-Lemma step_fupdN_nmono `{!invG Σ} E m n (P: iProp Σ) :
-  m ≤ n → (|={E}▷=>^m P) ={E}▷=∗^n P.
-Proof. generalize n.
-  elim m=> /=[|m']. { iIntros (? _) "?". by iApply step_fupdN_intro. }
+Section lemmas.
+Context `{BiFUpd PROP}.
+Implicit Type P Q: PROP.
+
+Lemma step_fupdN_nmono E m n P : m ≤ n → (|={E}▷=>^m P) -∗ (|={E}▷=>^n P).
+Proof.
+  move: n. elim m=> /=[|m']. { iIntros (? _) "?". by iApply step_fupdN_intro. }
   move=> IH ? /succ_le [n'[->Le]]/=. iIntros "?". by iApply IH.
 Qed.
+
+Lemma step_fupdN_combine n E P Q :
+  (|={E}▷=>^n P) -∗ (|={E}▷=>^n Q) -∗ (|={E}▷=>^n (P ∗ Q)).
+Proof.
+  elim n=> /=[|?]; [iIntros; by iFrame|]. iIntros (IH) ">UpdP >UpdQ !>!>".
+  by iMod (IH with "UpdP UpdQ") as "?".
+Qed.
+
+End lemmas.
